f3a25045c0cb22f0ff05df6f9cb672c08067aae5
Marcus Rands <mrands@hotmail.co.uk>
2017-09-14 14:26:56 +0100

    Refactored and created web_helper

--- spec/features/player_profile_spec.rb
+++ spec/features/player_profile_spec.rb
@@ -1,9 +1,6 @@
 RSpec.feature 'Players to update their profiles ' do
   scenario 'Fill in name form with John' do
-    visit('/')
-    fill_in :Player1, with: 'Marcus'
-    fill_in :Player2, with: 'James'
-    click_button 'Submit'
+    sign_in_and_play
     save_and_open_page
     expect(page).to have_content 'Marcus vs James'
   end
